fc8a76c371b3295e616a414ed1b76d4e668f844c
Hedrei Andrade <hedreiandrade@gmail.com>
2025-04-12 18:15:07 -0300

    melhorias no middlewire

--- app/Middlewares/Authenticate.php
+++ app/Middlewares/Authenticate.php
@@ -7,6 +7,8 @@ namespace App\Middlewares;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 class Authenticate
 {
@@ -14,12 +16,37 @@ class Authenticate
     {
         if (!$request->hasHeader('Authorization')) {
             $response->getBody()->write(json_encode([
-                'response' => 'Necessário token de acesso'
+                'response' => 'Please give me a token authorization.'
             ]));
             return $response->withStatus(401)
                            ->withHeader('Content-Type', 'application/json');
         }
-        
+        try {
+            $authHeader = $request->getHeader('Authorization')[0];
+            if (!preg_match('/Bearer\s(\S+)/', $authHeader, $matches)) {
+                $response->getBody()->write(json_encode([
+                    'response' => 'Invalid Token'
+                ]));
+                return $response->withStatus(401)
+                               ->withHeader('Content-Type', 'application/json');
+            }
+            $token = $matches[1];
+            try {
+                $decoded = JWT::decode($token, new Key(JWT_SECRET, 'HS256'));
+                $request = $request->withAttribute('token', $decoded);
+            } catch (\Exception $e) {
+                $response->getBody()->write(json_encode([
+                    'response' => 'Invalid Token',
+                ]));
+                return $response->withStatus(401)
+                            ->withHeader('Content-Type', 'application/json');
+            }
+
+        } catch (\Exception $e) {
+            $response->getBody()->write(json_encode([
+                'response' => 'Invalid Token'
+            ]));
+        }
         return $next($request, $response);
     }
 }
