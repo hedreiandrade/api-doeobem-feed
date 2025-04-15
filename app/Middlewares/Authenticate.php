e8cc4502daf9e8a229e13b5a93b70dd18c80472e
Hedrei Andrade <hedreiandrade@gmail.com>
2025-04-15 11:42:28 -0300

    melhoria middlewire

--- app/Middlewares/Authenticate.php
+++ app/Middlewares/Authenticate.php
@@ -20,30 +20,25 @@ class Authenticate
             ]));
             return $response->withStatus(401)->withHeader('Content-Type', 'application/json');
         }
-        try {
-            $authHeader = $request->getHeader('Authorization')[0];
-            if (!preg_match('/Bearer\s(\S+)/', $authHeader, $matches)) {
-                $response->getBody()->write(json_encode([
-                    'response' => 'Invalid Token'
-                ]));
-                return $response->withStatus(401)->withHeader('Content-Type', 'application/json');
-            }
-            $token = $matches[1];
-            try {
-                $decoded = JWT::decode($token, new Key(JWT_SECRET, 'HS256'));
-                $request = $request->withAttribute('token', $decoded);
-            } catch (\Exception $e) {
-                $response->getBody()->write(json_encode([
-                    'response' => 'Invalid Token',
-                ]));
-                return $response->withStatus(401)->withHeader('Content-Type', 'application/json');
-            }
 
-        } catch (\Exception $e) {
+        $authHeader = $request->getHeader('Authorization')[0];
+        if (!preg_match('/Bearer\s(\S+)/', $authHeader, $matches)) {
             $response->getBody()->write(json_encode([
                 'response' => 'Invalid Token'
             ]));
+            return $response->withStatus(401)->withHeader('Content-Type', 'application/json');
         }
+        $token = $matches[1];
+        try {
+            $decoded = JWT::decode($token, new Key(JWT_SECRET, 'HS256'));
+            $request = $request->withAttribute('token', $decoded);
+        } catch (\Exception $e) {
+            $response->getBody()->write(json_encode([
+                'response' => 'Invalid Token',
+            ]));
+            return $response->withStatus(401)->withHeader('Content-Type', 'application/json');
+        }
+
         return $next($request, $response);
     }
 }
